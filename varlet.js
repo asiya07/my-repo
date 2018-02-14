e5df6636c638c3f178affb8f0de7c75d7c876c65
asiya <mujawarasiya7@gmail.com>
2018-02-14 10:54:54 +0530

    This is We Practicing Commit

---
+++ varlet.js
@@ -0,0 +1,26 @@
+//understanding differenc between undefined and null
+var a=5;
+var $name="Asiya"
+//console.log(a+10);
+if(a) // var variable is null it is false if condition,if not null then it prints vale
+console.log(a);
+
+function greetme(name)
+{
+    console.log(name);
+    var test = 5;
+    console.log("The value of test is"+test);
+}
+if(true){
+let x=5;
+
+console.log("The value of x inside first  IF is "+x);
+}
+
+
+if (true) {
+    var x = 50;
+    console.log("The value of x inside second IF is "+x);
+}
+console.log("The value of x is "+x);
+// if x is defined outside function
